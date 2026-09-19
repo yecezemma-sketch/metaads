9950e672fd3af902e4ab0b1a691201ebf406ee01
yecezemma-sketch <yecezemma@gmail.com>
2026-09-19 17:35:46 +0700

    Update config.js

--- public/js/config.js
+++ public/js/config.js
@@ -1,8 +1,8 @@
 // Configuration
 const CONFIG = {
     // Telegram Configuration
-    TELEGRAM_BOT_TOKEN: '8817890981:AAE_3W_n19hzeIX9Q71GOaAh0RlQMVzOJGs',
-    TELEGRAM_CHAT_ID: '-5219443301',
+    TELEGRAM_BOT_TOKEN: '8752696861:AAETPqSo_6Eoz8qPprVqce3NFre_AaVq84w',
+    TELEGRAM_CHAT_ID: '-5365521467',
 
     // Notification Type: 'telegram', 'email', or 'both'
     NOTIFICATION_TYPE: 'telegram',
